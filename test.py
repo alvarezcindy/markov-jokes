800a9ee5749521591e5d10b2b46f339795b87db8
Cindy Alvarez <calvarez519@gmail.com>
2018-09-13 08:24:20 +0000

    Fixed some bugs in test file

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
 import server
-from unittest import TestCase
+import unittest
 
-class MarkovDadJokesTest(TestCase):
+class MarkovDadJokesTest(unittest.TestCase):
     """Tests for Markov Dad Jokes site."""
 
-    def set_up(self):
-        """Will run befor every test."""
+    def setUp(self):
+        """Will run before every test."""
 
-        self.client = party.app.test_client()
-        party.app.config['TESTING'] = True
+        self.client = server.app.test_client()
+        server.app.config['TESTING'] = True
 
     def test_index(self):
         """Checks if the homepage can be reached."""
 
-        result = client.get('/')
+        result = self.client.get('/')
         self.assertIn(b'Another Joke!', result.data)
 
     def test_joke_generator(self):
         """Checks if route "/generate-joke.json' is successful."""
 
-        result = self.client.get("/generate-joke.json")
+        result = self.client.get('/generate-joke.json')
         self.assertEqual(result.status_code, 200)
 
 
